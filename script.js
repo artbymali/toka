18ec6b527bd7477d6485bba59f7d81ddee119600
Muhammad ali <artbymali770@gmail.com>
2024-01-10 09:21:19 -0800

    somthing

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ function throttleScroll() {
 window.addEventListener("scroll", throttleScroll);
 
 menu.style.transition = 'background-color 0.3s ease'; 
+
+
+const scrollers = document.querySelectorAll(".Scroller__container");
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach((Scroller) => {
+        Scroller.setAttribute("data-animated", true);
+        
+         const scrollerInner = scroller.querySelectorAll(".Scroller__inner");
+         const scrollercontent = Array.from(scrollerInner.children);
+
+         scrollercontent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            console.log(duplicatedItem);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+         })
+    })
+}
